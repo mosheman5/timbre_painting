b9ea04bf010e498eac5e9a7d6d69fad5bdb14551
Michael Moshe <mosheman5@gmail.com>
2020-09-25 18:29:41 +0300

    remove crepe from requirements.txt and import to functions to codebase

--- data_utils/spectral_feats.py
+++ data_utils/spectral_feats.py
@@ -1,7 +1,6 @@
 import librosa
 import numpy as np
 import torch
-from crepe.core import to_local_average_cents, to_viterbi_cents
 
 from data_utils.crepe_pytorch import Crepe
 
@@ -149,64 +148,65 @@ def py_get_activation(audio, sr, model, center=False, step_size=10, layer=None, 
             return get_activation_inner(audio).detach()
 
 
-# def py_to_local_average_cents(salience, center=None):
-#     """
-#     find the weighted average cents near the argmax bin
-#     """
-#     if not hasattr(to_local_average_cents, 'cents_mapping'):
-#         # the bin number-to-cents mapping
-#         to_local_average_cents.mapping = torch.from_numpy(
-#                 np.linspace(0, 7180, 360) + 1997.3794084376191)
-#
-#     if salience.ndim == 1:
-#         if center is None:
-#             center = int(torch.argmax(salience))
-#         start = max(0, center - 4)
-#         end = min(len(salience), center + 5)
-#         salience = salience[start:end]
-#         product_sum = torch.sum(
-#             salience * to_local_average_cents.mapping[start:end])
-#         weight_sum = torch.sum(salience)
-#         return product_sum / weight_sum
-#     if salience.ndim == 2:
-#         return torch.cat([to_local_average_cents(salience[i, :]).view(1) for i in
-#                          range(salience.shape[0])])
-#
-#     raise Exception("label should be either 1d or 2d ndarray")
+def to_local_average_cents(salience, center=None):
+    """
+    find the weighted average cents near the argmax bin
+    """
+
+    if not hasattr(to_local_average_cents, 'cents_mapping'):
+        # the bin number-to-cents mapping
+        to_local_average_cents.cents_mapping = (
+                np.linspace(0, 7180, 360) + 1997.3794084376191)
+
+    if salience.ndim == 1:
+        if center is None:
+            center = int(np.argmax(salience))
+        start = max(0, center - 4)
+        end = min(len(salience), center + 5)
+        salience = salience[start:end]
+        product_sum = np.sum(
+            salience * to_local_average_cents.cents_mapping[start:end])
+        weight_sum = np.sum(salience)
+        return product_sum / weight_sum
+    if salience.ndim == 2:
+        return np.array([to_local_average_cents(salience[i, :]) for i in
+                         range(salience.shape[0])])
+
+    raise Exception("label should be either 1d or 2d ndarray")
 
 
-# def to_viterbi_cents(salience):
-#     """
-#     Find the Viterbi path using a transition prior that induces pitch
-#     continuity.
-#     """
-#     from hmmlearn import hmm
+def to_viterbi_cents(salience):
+    """
+    Find the Viterbi path using a transition prior that induces pitch
+    continuity.
+    """
+    from hmmlearn import hmm
 
-#     # uniform prior on the starting pitch
-#     starting = np.ones(360) / 360
+    # uniform prior on the starting pitch
+    starting = np.ones(360) / 360
 
-#     # transition probabilities inducing continuous pitch
-#     xx, yy = np.meshgrid(range(360), range(360))
-#     transition = np.maximum(12 - abs(xx - yy), 0)
-#     transition = transition / np.sum(transition, axis=1)[:, None]
+    # transition probabilities inducing continuous pitch
+    xx, yy = np.meshgrid(range(360), range(360))
+    transition = np.maximum(12 - abs(xx - yy), 0)
+    transition = transition / np.sum(transition, axis=1)[:, None]
 
-#     # emission probability = fixed probability for self, evenly distribute the
-#     # others
-#     self_emission = 0.1
-#     emission = (np.eye(360) * self_emission + np.ones(shape=(360, 360)) *
-#                 ((1 - self_emission) / 360))
+    # emission probability = fixed probability for self, evenly distribute the
+    # others
+    self_emission = 0.1
+    emission = (np.eye(360) * self_emission + np.ones(shape=(360, 360)) *
+                ((1 - self_emission) / 360))
 
-#     # fix the model parameters because we are not optimizing the model
-#     model = hmm.MultinomialHMM(360, starting, transition)
-#     model.startprob_, model.transmat_, model.emissionprob_ = \
-#         starting, transition, emission
+    # fix the model parameters because we are not optimizing the model
+    model = hmm.MultinomialHMM(360, starting, transition)
+    model.startprob_, model.transmat_, model.emissionprob_ = \
+        starting, transition, emission
 
-#     # find the Viterbi path
-#     observations = np.argmax(salience, axis=1)
-#     path = model.predict(observations.reshape(-1, 1), [len(observations)])
+    # find the Viterbi path
+    observations = np.argmax(salience, axis=1)
+    path = model.predict(observations.reshape(-1, 1), [len(observations)])
 
-#     return np.array([to_local_average_cents(salience[i, :], path[i]) for i in
-#                      range(len(observations))])
+    return np.array([to_local_average_cents(salience[i, :], path[i]) for i in
+                     range(len(observations))])
 
 
 def predict(audio, sr, model, viterbi=False, center=False, step_size=10, confidence_th=-1, sampler=None):
